a7b176c4c39e836da48a194699c52774f911b62c
Nicolas <109254154+kZNick@users.noreply.github.com>
2022-11-08 19:39:51 -0300

    Rederizando os pets q vc tem

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -1,3 +1,4 @@
+import {usuario} from "./informacaoUser.js"
 import { atualizarPerfil, petCadastro, petAtualizar, deletarConta } from "./modalProfile.js";
 
 atualizarPerfil()
@@ -8,44 +9,6 @@ petAtualizar()
 
 deletarConta()
 
-
 const token = localStorage.getItem("userToken")
 
-async function usuario (){
-
-    const user = await fetch("https://m2-api-adot-pet.herokuapp.com/users/profile", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          }
-        });
-        const userJson = await user.json();
-        console.log(userJson)
-
-        const fotoPerfil = document.querySelector(".img-perfil")
-        fotoPerfil.src = userJson.avatar_url
-
-        const infoUsuario = document.querySelector(".info-usuario")
-
-        infoUsuario.innerHTML = `
-        
-        <p><span>Nome:</span> ${userJson.name}</p>
-        <p><span>E-mail:</span> ${userJson.email}</p>
-        `
-
-}
-usuario ()
-
-
-
-const fotoPerfil = document.querySelector(".caixa-img-perfil")
-
-fotoPerfil.addEventListener("click",()=>{
-    const editarPerfil = document.querySelector(".info-usuarios")
-    editarPerfil.classList.toggle("info-usuarios-on")
-
-})
-
-
-
+usuario (token)
